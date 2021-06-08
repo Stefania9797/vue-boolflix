e641b5fdc08e84c9d2a1f6ddc876c18f7a28061c
Stefania9797 <stefaniamastrel@gmail.com>
2021-06-08 12:32:29 +0200

    ms-2 init

---
+++ milestone-2/assets/main.js
@@ -0,0 +1,19 @@
+let root=new Vue({
+    el:"#root",
+    data:{
+        apiKey:"b7dde1ec04c6c67918064ecac356278b",
+        searchResults:null,
+        searchInput:""
+    },
+    methods:{
+        searchMovies() {
+            if (this.searchInput != '') {
+              axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${this.apiKey}&query=${this.searchInput}`)
+                .then((response) => {
+                  this.searchResults= response.data.results;
+                });
+            }
+          }
+    },
+    mounted(){}
+})
