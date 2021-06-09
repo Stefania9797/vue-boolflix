632ed7d6336946da47f398957a5b6be3bdb3e230
Stefania9797 <stefaniamastrel@gmail.com>
2021-06-09 14:55:38 +0200

    ms-4 init

---
+++ milestone-4/assets/main.js
@@ -0,0 +1,30 @@
+let root=new Vue({
+    el:"#root",
+    data:{
+        apiKey:"b7dde1ec04c6c67918064ecac356278b",
+        searchResults:null,
+        searchInput:"",
+        flags:["en","it","fr","de","fi","ja","es"]
+    },
+    methods:{
+        getResults() {
+            if (this.searchInput != '') {
+              axios.get(`https://api.themoviedb.org/3/search/multi?api_key=${this.apiKey}&query=${this.searchInput}`)
+                .then((response) => {
+                  this.searchResults= response.data.results;
+                });
+            }
+          },
+          getFlag(language){
+            return "./assets/flags/"+language+".png"
+          },
+          getPoster(img){
+            return "https://image.tmdb.org/t/p/w185"+img
+          },
+          getStars(voto) {
+            return Math.ceil(voto / 2);
+        },
+    },
+    mounted(){
+    }
+})
